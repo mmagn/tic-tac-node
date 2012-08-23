1ca6db4f091ace0fb309cbbd644906cc00ac1b11
unknown <mathieu.magnin@gmail.com>
2012-08-23 19:07:07 +0200

    add dependencies

--- static/js/client.js
+++ static/js/client.js
@@ -1,27 +1,26 @@
-
-var renderBoard = function(gameState){
-	var x=0, player=0, board = $('<p>');
-
-	for(var i=0; i<gameState.grid.length; i++){
-		for(var j=0; j<gameState.grid[i].length; j++){
-
-			var elt = $('<a>');
-			player = gameState.grid[i][j];
-			elt.addClass(player ? "player" + player : "");
-			elt.attr("data-position", "["+i+","+j+"]");
-			board.append(elt);
-			x++;
-
-		}
-	}
-	return board;
-};
-
-var whoToPlay = function(identity, gameState){
-	return identity.number === gameState.nextPlayer ? "It's your turn." : "Opponent turn.";
-};
-
 $(function(){
+	var renderBoard = function(gameState){
+		var x=0, player=0, board = $('<p>');
+
+		for(var i=0; i<gameState.grid.length; i++){
+			for(var j=0; j<gameState.grid[i].length; j++){
+
+				var elt = $('<a>');
+				player = gameState.grid[i][j];
+				elt.addClass(player ? "player" + player : "");
+				elt.attr("data-position", "["+i+","+j+"]");
+				board.append(elt);
+				x++;
+
+			}
+		}
+		return board;
+	};
+
+	var whoToPlay = function(identity, gameState){
+		return identity.number === gameState.nextPlayer ? "It's your turn." : "Opponent turn.";
+	};
+	
 	var identity,
 		gameState,
 		$gameBoard = $('#gameBoard'),
